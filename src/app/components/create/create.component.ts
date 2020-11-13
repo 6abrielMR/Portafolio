dbbb46e1f67c6eb77bb94bc8dc76830cd96e6b55
Gabriel Montes <gabriel.montes716@gmail.com>
2020-11-13 00:50:14 -0500

    Se agrego la funcionalidad para subir imagenes de cada proyecto creado

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../models/project';
 import { ProjectService } from '../../services/project.service';
+import { UploadService } from '../../services/upload.service';
+import { Global } from '../../services/global';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.sass'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title: string;
   public project: Project;
   public status: string;
+  public filesToUpload: Array<File>;
 
   constructor(
-    private _projectService: ProjectService
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
   ) {
     this.title = 'Crear Proyecto';
     this.project = new Project('', '', '', '', 2020, '', '');
@@ -32,14 +36,24 @@ export class CreateComponent implements OnInit {
   }
 
   onSubmit(form) {
+    //Guardar datos básicos
     this.status = 'initiated';
     this._projectService.saveProject(this.project).subscribe(
       response => {
         console.log(response);
         if (response.project) {
-          this.status = 'success';
-          form.reset();
-          this.hideMessageStatus(3000);
+          //Subir la imagen
+          this._uploadService.makeFileRequest(
+            Global.url + "upload-image/" + response.project._id,
+            [],
+            this.filesToUpload,
+            'image'
+          ).then((result: any) => {
+            this.status = 'success';
+            console.log(result);
+            form.reset();
+            this.hideMessageStatus(3000);
+          });
         }
         else {
           this.status = 'failed';
@@ -51,6 +65,10 @@ export class CreateComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
